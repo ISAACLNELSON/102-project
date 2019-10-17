7d54e11fb089dca5e4cbdbff2613c30ac4088e55
Isaac <isaacschoolacc@gmail.com>
2019-10-17 10:25:48 -0700

    added buttons to display boxes

---
+++ onClick.js
@@ -0,0 +1,10 @@
+function showData(){
+    document.getElementById('dataBox').style.display = 'block';
+}
+
+function showCon(){
+    document.getElementById('consultBox').style.display = 'block';
+}
+function showRep(){
+    document.getElementById('reportBox').style.display = 'block';
+}
