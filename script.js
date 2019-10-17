02117f3d6d56e3ce1924685186c737cec961f603
Isaac <isaacschoolacc@gmail.com>
2019-10-17 09:03:18 -0700

    added js file and updated css and html

---
+++ script.js
@@ -0,0 +1,7 @@
+//get todays date
+var today = new Date();
+//get current timein hours
+var hour = today.getHours();
+//if current hour is either morning, change bg to yellow afternoon blue, or evening purple
+
+document.write
